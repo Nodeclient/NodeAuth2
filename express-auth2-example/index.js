1d2e4e2e8ec8c6b0bac668573c7e0f3f2a758c13
Nodeclient <41586831+Nodeclient@users.noreply.github.com>
2020-06-02 23:50:10 +0300

    na2 http demo
    example project

---
+++ express-auth2-example/index.js
@@ -0,0 +1,52 @@
+
+const ejs = require('ejs');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+	app.use( bodyParser.json() );
+		app.use(bodyParser.urlencoded({
+			extended: true
+	})); 
+
+	//INCLUDE TIME SERVICE
+	const ns = require('daytime-service').default
+	const daytime = new ns.DayTime("NA2");
+	//http://yourhost/api/daytime
+	app.get('/api/daytime', (req, res) => res.send( daytime.GetTime ))
+
+	// NODEAUTH2
+	process.env.TOKEN_LENGTH = "6"; // Token length [123456] Min(4) ~ Max(32)
+	process.env.TOKEN_PREFIX = "-"; // Custom token prefix (-) (*) (🔑)
+	const na2 = require("nodeauth2"); // Import NA2 Module
+	const NodeAuth2 = new na2.default.Authentication(20); // Token Expiration Time 20 sec
+	NodeAuth2.http = "http://127.0.0.1:3000/api/daytime"
+	
+	//LOGIN PAGE
+	app.get("/login", function(req,res) {
+		res.render('./pages/login.ejs')
+	})
+	
+	/* DATABASE SIMULATION */
+		let DB = { mail:"test@example.com",secret:"this is your secret pass phrase" }
+
+	//CHECK TOKEN
+	app.post('/one-time-check', function(req, res) {
+		var user_id = req.body["inputEmail"] ;
+		var token = req.body["inputToken"] ;
+			if(user_id == DB.mail){
+
+				NodeAuth2.AuthCheck(DB.secret,token).then( t =>{
+					console.log(t , DB.secret);
+						res.render('./pages/page.ejs', { user: user_id , data: t } );
+				});
+
+			} else{
+				res.send( "User not Found : " + user_id )
+			}
+	});
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
---
+++ express-auth2-example/readme.md
@@ -0,0 +1,19 @@
+# NodeAuth2 (Example Auth Project)
+![npm-image]
+![npm](https://img.shields.io/npm/dt/nodeauth2)
+
+
+## Installation
+```bash
+ git clone https://github.com/Nodeclient/NodeAuth2.git
+ cd express-auth2-example
+ npm install
+ npm run index.js
+```
+
+## Auth2 Browser Url
+> http://127.0.0.1:3000/login
+
+
+   [npm-image]: https://img.shields.io/npm/v/nodeauth2.svg?style=flat 
+   [npm-url]: https://npmjs.org/package/nodeauth2 
